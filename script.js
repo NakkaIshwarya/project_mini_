65bae98f5d1607308e8164f2a96d9d6eb01d872e
Ishwarya <nakkaishwarya2@gmail.com>
2025-11-12 10:00:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+gsap.from(".nav_bar",{
+    duration:1,
+    delay:0,
+    opacity:0,
+    y:30,  
+    stagger: 0.3,
+})
+gsap.from(".explore_world",{
+    duration:1,
+    delay:0,
+    opacity:0,
+    x:-30,
+    stagger: 0.3,
+})
+gsap.from(".depature_arival_check",{
+    duration:1,
+    delay:0,
+    opacity:0,
+    x:30,
+    stagger: 0.3,
+})
+gsap.from(".content1 *",{
+    duration:1,
+    delay:2,
+    opacity:0,
+    y:30,
+    stagger: 0.3,
+})
+gsap.from(".contents *",{
+    duration:1,
+    delay:3,
+    opacity:0,
+    x:-20,
+    y:30,
+    stagger: 0.3,
+})
+gsap.from(".contentss *",{
+    duration:1,
+    delay:4,
+    opacity:0,
+    x:-20,
+    y:30,
+    stagger: 0.3,
+})
+gsap.from(".img1",{
+    duration:1,
+    delay:8,
+    opacity:0,
+    x:-20,
+    stagger: 0.3,
+})
+gsap.from(".mat",{
+    duration:1,
+    delay:8,
+    opacity:0,
+    x:20,
+    stagger: 0.3,
+})
+gsap.from(".img11",{
+    duration:1,
+    delay:9,
+    opacity:0,
+    x:20,
+    stagger: 0.3,
+})
+gsap.from(".mat1",{
+    duration:1,
+    delay:9,
+    opacity:0,
+    x:-20,
+    stagger: 0.3,
+})
+
